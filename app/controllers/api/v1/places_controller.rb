8fc9769ad42207806252aeee14ef6ea7d5e33e07
Andrew Stilmock <andrew.stilmock@gmail.com>
2023-08-28 16:46:58 -0600

    /search route update for Place service

--- app/controllers/api/v1/places_controller.rb
+++ app/controllers/api/v1/places_controller.rb
@@ -1,5 +1,13 @@
 class  Api::V1::PlacesController < ApplicationController
   def index
+    if params[:search] == "random"
+      location = LocationFacade.new.get_coords(params[:query])
+      fetch_place_photo = PlaceFacade.new(location).find_random_restaurants
+      render json: PlaceSerializer.new(fetch_place_photo)
+    end
+  end
+
+  def search
     fetch_place = PlaceFacade.new(params[:query])
     fetch_place_photo = fetch_place.find_place
     if fetch_place_photo != []
@@ -7,12 +15,5 @@ class  Api::V1::PlacesController < ApplicationController
     else
       render json: "no restaurants located with search query", status: 404
     end
-    if params[:search] == "random"
-      location = LocationFacade.new.get_coords(params[:query])
-      fetch_place_photo = PlaceFacade.new(location).find_random_restaurants
-    else
-      fetch_place_photo = PlaceFacade.new(params[:query]).find_place
-    end
-    render json: PlaceSerializer.new(fetch_place_photo)
   end
 end
