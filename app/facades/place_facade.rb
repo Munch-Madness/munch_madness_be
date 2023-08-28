be4c1f638d3d62df4813465b5983046fde7042d5
Andrew Stilmock <andrew.stilmock@gmail.com>
2023-08-28 15:04:36 -0600

    Add sad paths/error responses to facade controller

--- app/facades/place_facade.rb
+++ app/facades/place_facade.rb
@@ -9,8 +9,15 @@ class PlaceFacade
 
   def find_place
     place = place_service.find_place(@query)
-    place_ref = place[:candidates].first[:photos].first[:photo_reference]
-    photo = place_service.find_photo(place_ref)
-    place_item = Place.new(place[:candidates].first[:name], photo.env[:response_headers][:location])
+    if place[:candidates].empty?
+      []
+    elsif
+      place[:candidates].first[:photos].nil?
+      []
+    else
+      place_ref = place[:candidates].first[:photos].first[:photo_reference]
+      photo = place_service.find_photo(place_ref)
+      place_item = Place.new(place[:candidates].first[:name], photo.env[:response_headers][:location])
+    end
   end
 end
