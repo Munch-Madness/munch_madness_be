48b35d83221efdca9e8010a30508db5d81801a19
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-08-28 16:00:33 -0600

    add to vcr config

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -70,6 +70,7 @@ VCR.configure do |config|
   config.hook_into :webmock
   config.filter_sensitive_data('google_key') { ENV['rest_key'] }
   config.configure_rspec_metadata!
+  config.default_cassette_options = { record: :new_episodes }
 end
 
 Shoulda::Matchers.configure do |config|
