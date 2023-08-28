e6f13bdffb0457a82429fc53c292d5fa5497a622
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-08-28 15:33:43 -0600

    update places controller

--- app/controllers/api/v1/places_controller.rb
+++ app/controllers/api/v1/places_controller.rb
@@ -1,6 +1,7 @@
 class  Api::V1::PlacesController < ApplicationController
   def index
     fetch_place_photo = PlaceFacade.new(params[:query]).find_place
+    require 'pry'; binding.pry
     render json: PlaceSerializer.new(fetch_place_photo)
   end
 end
