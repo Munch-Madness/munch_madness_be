8790803d6c163bd9c09a4688332ea42470616004
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-08-28 15:54:15 -0600

    fix vcr rails helper, move things in circleci

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -68,7 +68,7 @@ end
 VCR.configure do |config|
   config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
   config.hook_into :webmock
-  config.filter_sensitive_data('tmdb_key') { ENV['google_key'] }
+  config.filter_sensitive_data('google_key') { ENV['rest_key'] }
   config.configure_rspec_metadata!
 end
 
