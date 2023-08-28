3ef807199231b8496c3da2ec4c9cbd66da919dbe
Lauren Golden <llgolden@gmail.com>
2023-08-28 16:00:16 -0600

    Update controller action to include random search

--- app/services/place_service.rb
+++ app/services/place_service.rb
@@ -21,8 +21,7 @@ class PlaceService
   end
 
   def random_restaurants(location)
-    # require 'pry'; binding.pry
     key = ENV["rest_key"]
-    get_url("/maps/api/place/nearbysearch/json?location=#{location.latitude.to_f},#{location.longitude.to_f}&radius=1500&type=restaurant&key=#{key}")
+    get_url("/maps/api/place/nearbysearch/json?location=#{location.latitude.to_f},#{location.longitude.to_f}&radius=50000&type=restaurant&key=#{key}")
   end
 end
