a22e61310321f8f4661036ca83934d5bef5b9ef6
Andy Weissman <andyweissman6@gmail.com>
2023-08-28 14:46:35 -0600

    wip

--- app/controllers/api/v1/places_controller.rb
+++ app/controllers/api/v1/places_controller.rb
@@ -1,5 +1,6 @@
 class  Api::V1::PlacesController < ApplicationController
   def index
+    require 'pry'; binding.pry
     fetch_place_photo = PlaceFacade.new(params[:query]).find_place
     render json: PlaceSerializer.new(fetch_place_photo)
   end
--- spec/requests/api/v1/places_search_spec.rb
+++ spec/requests/api/v1/places_search_spec.rb
@@ -30,5 +30,34 @@ RSpec.describe "Get Place Endpoint" do
       expect(place_data[:data][:attributes]).to have_key(:photo)
       expect(place_data[:data][:attributes][:photo]).to be_a(String)
     end
+
+    it "returns a list of random restaurants", :vcr do
+      query = "20817"
+      get "/api/v1/places", params: { query: query }
+
+      place_data = JSON.parse(response.body, symbolize_names: true)
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
+      
+      expect(place_data).to be_a(Hash)
+      expect(place_data).to have_key(:data)
+
+      expect(place_data[:data]).to be_a(Hash)
+      
+      expect(place_data[:data]).to have_key(:id)
+      expect(place_data[:data][:id]).to be_a(String)
+
+      expect(place_data[:data]).to have_key(:type)
+      expect(place_data[:data][:type]).to be_a(String)
+      
+      expect(place_data[:data]).to have_key(:attributes)
+      expect(place_data[:data][:attributes]).to be_a(Hash)
+      
+      expect(place_data[:data][:attributes]).to have_key(:name)
+      expect(place_data[:data][:attributes][:name]).to be_a(String)
+      
+      expect(place_data[:data][:attributes]).to have_key(:photo)
+      expect(place_data[:data][:attributes][:photo]).to be_a(String)
+    end
   end
 end
